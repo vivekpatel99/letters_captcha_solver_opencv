acb6c97e045434dec87696abb3f90b9b0c3665be
vivekpatel99 <vivpatel99@gmail.com>
2023-01-28 13:40:48 +0100

    script done

--- find_param.py
+++ find_param.py
@@ -1,5 +1,10 @@
 import cv2
 
+"""
+This script is help to find color range for target object. 
+In this case, we can try to manipulate Trackbar until we can see only Numbers without any noise or color
+"""
+
 
 def empty(a):
     pass
@@ -13,31 +18,39 @@ def find_param(captcha: str = 'captchas/captcha_0.png'):
 
     # in Real world image color does not have same value, because of shadows and noise
     # hence we have to use range of colors
-    hmin = 0
-    smin = 0
-    vmin = 128
+    h_min = 0  # 0
+    s_min = 0  # 0
+    v_min = 0  # 101
 
-    hmax = 255
-    smax = 255
-    vmax = 255
+    h_max = 255  # 179
+    s_max = 255  # 255
+    v_max = 255  # 255
 
     # A window with 'Display' name is created
     # with WINDOW_AUTOSIZE, window size is set automatically
     cv2.namedWindow("Parameters", cv2.WINDOW_AUTOSIZE)
-    cv2.createTrackbar("hue", "Parameters", 179, 255, empty)
-    cv2.createTrackbar("sat", "Parameters", 179, 255, empty)
-    cv2.createTrackbar("val", "Parameters", 179, 255, empty)
+    cv2.createTrackbar("Hue Min", "Parameters", 0, 179, empty)
+    cv2.createTrackbar("Hue Max", "Parameters", 179, 179, empty)
+    cv2.createTrackbar("Sat Min", "Parameters", 0, 255, empty)
+    cv2.createTrackbar("Sat Max", "Parameters", 255, 255, empty)
+    cv2.createTrackbar("Val Min", "Parameters", 0, 255, empty)
+    cv2.createTrackbar("Val Max", "Parameters", 255, 255, empty)
 
     while True:
-        hue = cv2.getTrackbarPos("hue", "Parameters")
-        sat = cv2.getTrackbarPos("sat", "Parameters")
-        val = cv2.getTrackbarPos("val", "Parameters")
+        h_min = cv2.getTrackbarPos("Hue Min", "Parameters")
+        h_max = cv2.getTrackbarPos("Hue Max", "Parameters")
+        s_min = cv2.getTrackbarPos("Sat Min", "Parameters")
+        s_max = cv2.getTrackbarPos("Sat Max", "Parameters")
+        v_min = cv2.getTrackbarPos("Val Min", "Parameters")
+        v_max = cv2.getTrackbarPos("Val Max", "Parameters")
 
-        img_canny = cv2.Canny(img_hsv, )
-        cv2.imshow("Image", img)
-        cv2.imshow("imgHSV", img_hsv)
-        cv2.imshow("mask", mask)
+        lower_limits = (h_min, s_min, v_min)
+        upper_limits = (h_max, s_max, v_max)
+        mask = cv2.inRange(img_hsv, lower_limits, upper_limits)
+
         cv2.imshow('captcha', captcha_img)
+        cv2.imshow("img_hsv", img_hsv)
+        cv2.imshow("mask", mask)
 
         # Waiting 0ms for user to press any key
         if cv2.waitKey(1) & 0xFF == ord('q'):
--- main.py
+++ main.py
@@ -1,16 +1,56 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import cv2
+import easyocr
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main(captcha: str = 'captchas/captcha_1.png'):
+    captcha_img = cv2.imread(captcha)
+
+    scale = 4
+    width = int(captcha_img.shape[1] * scale)
+    height = int(captcha_img.shape[0] * scale)
+    dim = (width, height)
+
+    captcha_img = cv2.resize(captcha_img, dim, interpolation=cv2.INTER_AREA)
+    # Convert image into HSV space
+    img_hsv = cv2.cvtColor(captcha_img, cv2.COLOR_BGR2HSV)
+
+    # these value can be found with help of find_param script
+    h_min = 0
+    s_min = 0
+    v_min = 101
+
+    h_max = 179
+    s_max = 255
+    v_max = 255
+
+    lower_limits = (h_min, s_min, v_min)
+    upper_limits = (h_max, s_max, v_max)
+
+    mask = cv2.inRange(img_hsv, lower_limits, upper_limits)
+
+    # instance text detector
+    reader = easyocr.Reader(['en'], gpu=False)
+    # detect text on image
+    text_ = reader.readtext(mask)
+    threshold = 0.25
+    for t_, t in enumerate(text_):
+        print(text_)
+
+        bbox, text, score = t
+        print(bbox)
+        if score > threshold:
+            cv2.rectangle(captcha_img, bbox[0], bbox[2], (255, 0, 255), 2)
+            cv2.putText(captcha_img, text, bbox[0], cv2.FONT_HERSHEY_COMPLEX, 0.70, (255, 0, 255), 2)
+
+    cv2.imshow('captcha', captcha_img)
+    cv2.imshow("img_hsv", img_hsv)
+    cv2.imshow("mask", mask)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
